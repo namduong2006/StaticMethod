753c2230f0fc52280fdb2a0730a9d7dfeb5fe790
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-21 20:46:04 +0700

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    internal class Student
+    {
+        private int rollno;
+        private string name;
+        private static string college = "BBDIT";
+        public Student(int r, string n)
+        {
+            rollno = r;
+            name = n;
+        }
+        public static void Change()
+        {
+            college = "CODEGYM";
+        }
+        public void Display()
+        {
+            Console.WriteLine(rollno + " " + name + " " + college);
+        }
+    }
+}
